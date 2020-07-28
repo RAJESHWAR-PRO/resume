fef4b1c4fa0401ccf705b0fadf09aeda24f32d09
RAJESHWAR-PRO <44514770+RAJESHWAR-PRO@users.noreply.github.com>
2020-07-28 15:12:10 +0530

    Removed unmatched imports

--- src/app/Education/education.component.ts
+++ src/app/Education/education.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, HostListener, Inject } from '@angular/core';  
-import { trigger, state, transition, style, animate } from '@angular/animations';  
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit } from '@angular/core';  
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
